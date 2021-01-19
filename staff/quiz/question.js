d3a9d6d143f6d19c821cc34310b0fce8b08ff610
davtonlearn <davtonlms@gmail.com>
2021-01-19 14:04:02 +0000

    up

---
+++ staff/quiz/question.js
@@ -0,0 +1,16 @@
+var questions = [{
+	"question": "The first mechanical computer designed by Charles Babbage was called ?",
+	"option1": "Abacus",
+	"option2": "Analytical Engine",
+	"option3": "Calculator",
+	"option4": "Processor",
+	"answer": "2"
+}, {
+	"question": "Which of the following is the most powerful type of computer ?",
+	"option1": "Super-micro",
+	"option2": "Super conductor",
+	"option3": "Super computer",
+	"option4": "Megaframe",
+	"answer": "3"
+}
+];
